2316339887b11b31df956acc550e9031727b6a5d
uls19 <uls19@mail.ru>
2023-08-29 00:09:23 +0200

    slider-carousel

--- script.js
+++ script.js
@@ -117,7 +117,100 @@ window.onload = function () {
         });
         label03Off.style.display = 'none';
 
-    };
+    }; //     // box5.onmouseout = function (e) {  }; //при отведении курсора 
+};  
 
-};          //     // box5.onmouseout = function (e) {  }; //при отведении курсора
+//слайдер-карусель
+var fragPre = document.createDocumentFragment(),
+fragPost = document.createDocumentFragment(),
+clonedPre, clonedPost,
+selectSlide = document.getElementById("numberOfSlides"),
+
+//options
+numberOfSlides = 4;
+
+
+var items   = document.querySelectorAll("#gallery .events-box .events-box__item"),
+len     = items.length,
+current = 1,  /* текущий элемент, который мы ищем */
+wrapper = document.getElementById("wrapper"),
+transformVal = 0;
+
+
+/* 1. Клонирование последних элементов и добавление к первым */  
+
+for(var i=numberOfSlides ; i > 0 ; i--) {
+clonedPre = items[items.length-i].cloneNode(true);
+fragPre.append(clonedPre);
+}
+
+wrapper.insertBefore(fragPre , items[0]);
+
+/* . Клонирование первых элементов и добавление к первым */  
+
+for(var j = 0 ; j <= numberOfSlides-1 ; j++) {
+clonedPost = items[j].cloneNode(true);
+fragPost.append(clonedPost);
+}
+
+wrapper.appendChild(fragPost);
+
+/* Slider arrow click function */  
+
+var slideWidth=items[0].offsetWidth;
+var counter = 0;
+var timer = null;
+var timeout = null;
+wrapper.style.transform = "translate3d(" + (-slideWidth) * (numberOfSlides) + "px,0,0)";
+
+function arrowClick(dir) {
+// если тайм-аут || таймер не равен нулю, чем сейчас обрабатывается скольжение, в этом случае запретите обработку действий пользователя
+if (timeout || timer) {
+  return;
+}
+timeout = setTimeout(function() {
+    counter = 0;
+    direction = dir;
+    var str = wrapper.style.transform;
+    var left = str.substring(12, str.length - 11);
+    console.log(left);
+    animateSlide(current, left);
+    timeout = null; // <-- установка таймаута в ноль, что будет означать, что функция частично (также проверит таймер) готова к действиям нового пользователя
+}, 100);
+}
+
+/* slide number click function */  
+
+function changeCurrent(curr) {
+current = curr;
+wrapper.style.transform =  "translate3d(" + -(slideWidth) * (current + (numberOfSlides-1)) + "px,0,0)";
+}
+
+
+/* actual sliding effect */  
+
+function animateSlide(curr, left) {
+// if timer is not prevent processing new animation
+if (timer) {
+  return;
+}
+timer = setInterval(function() {
+    if(counter < slideWidth) {
+        counter += 2; // <-- this is the speed of animation. bigger number faster animating
+        transformVal = parseInt(left, 10) + (-(counter) * direction);
+        wrapper.style.transform = "translate3d(" + transformVal + "px,0,0)";
+    } else {
+        current += direction;
+        cycle = !!(current === 0 || current > len);
+        if (cycle) {
+          current = (current === 0)? len : 1;
+          wrapper.style.transform ="translate3d(" + (-(slideWidth) * (current + (numberOfSlides-1))) + "px,0,0)";
+        }
+        counter = 0;
+        clearInterval(timer);
+        timer = null; // <-- setting timer to null and now it's ready for another animaton
+    }
+}, 0);  
+}
+
 
